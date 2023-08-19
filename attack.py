0d34266d44c28c256b22f0504c5625472573f699
Renotip <138821317+renotip@users.noreply.github.com>
2023-08-19 11:18:40 +0500

    Add files via upload

---
+++ attack.py
@@ -0,0 +1,21 @@
+renotip=input("""\033[1;34m
+╔═══╦═══╦═╗─╔╦═══╦════╦══╦═══╗
+║╔═╗║╔══╣║╚╗║║╔═╗║╔╗╔╗╠╣╠╣╔═╗║
+║╚═╝║╚══╣╔╗╚╝║║─║╠╝║║╚╝║║║╚═╝║
+║╔╗╔╣╔══╣║╚╗║║║─║║─║║──║║║╔══╝
+║║║╚╣╚══╣║─║║║╚═╝║─║║─╔╣╠╣║
+╚╝╚═╩═══╩╝─╚═╩═══╝─╚╝─╚══╩╝
+\033[1;33mTELEGRAM KANAL===> @xakkerjamoa
+
+BITTASINI TANLANG
+
+1 DDOS QILISH==>
+2 SPAM ATTCK==>
+3 DASTURCHI==>
+Shulardan birini tanlang==>""")
+if renotip=='1':
+    input('Ip manzilni kiriting --> ')
+    print('DDOS BOSHLANDI')
+if renotip=='2':
+	input('Chat Userini kiriting --> ')
+	print('Qabul qilindi Hujum boshlandi!!!')    
